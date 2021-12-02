766d6f6922df40da3d0c8ea6c9136990648a2ea6
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2021-12-02 21:05:19 +0300

    Добавьте файлы проекта.

---
+++ Task5-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int sum = 0;
+            int arithmeticMean = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());
+                sum += array[i];
+                arithmeticMean = sum / 7;
+            }
+            Console.WriteLine("Среднее арифметическое равно {0}", arithmeticMean);
+            Console.ReadKey();
+        }
+    }
+}
